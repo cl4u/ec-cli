074ccca68b8b03fafef6d2c02563602f12842c76
任轲 <renke@fpwis.com>
2024-03-06 17:22:15 +0800

    代码开发

---
+++ bin/cli.js
@@ -0,0 +1,44 @@
+#! /usr/bin/env node
+
+const program = require("commander");
+const chalk = require("chalk");
+const figlet = require("figlet");
+
+program
+  // 定义命令和参数
+  .command("create <app-name>")
+  .description("create a new project")
+  // -f or --force 为强制创建，如果创建的目录存在则直接覆盖
+  .option("-f,--force", "overwrite target directory if it exist")
+  .action((name, options) => {
+    // 在create.js中执行创建任务
+    require("../lib/create.js")(name, options);
+  });
+
+program
+  // 配置版本号信息
+  .version(`v${require("../package.json").version}`)
+  .usage("<command> [option]");
+
+program.on("--help", () => {
+  // 使用figlet绘制logo
+  console.log(
+    "\r\n" +
+      figlet.textSync("ec", {
+        font: "Ghost",
+        horizontalLayout: "default",
+        verticalLayout: "default",
+        width: 80,
+        whitespaceBreak: true,
+      })
+  );
+  // 新增说明信息
+  console.log(
+    `\r\nRun ${chalk.cyan(
+      `ec <command> --help`
+    )} for detailed usage of given command\r\n`
+  );
+});
+
+// 解析用户执行命令传入参数
+program.parse(process.argv);
